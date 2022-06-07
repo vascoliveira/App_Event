204d920b80bd562c86c58b513b8f4d4f625aed9f
vasco <vasco144oliveira@gmail.com>
2022-06-07 18:50:56 +0100

    criacao da classes organizadores, eventos,localidades bem como os seus atributos

--- app/src/main/java/pt/ipg/appevent/TabelaBDEvento.kt
+++ app/src/main/java/pt/ipg/appevent/TabelaBDEvento.kt
@@ -1,4 +1,19 @@
 package pt.ipg.appevent
 
-class TabelaBDEvento {
+import android.database.sqlite.SQLiteDatabase
+import android.provider.BaseColumns
+
+class TabelaBDEvento(db: SQLiteDatabase) : TabelaBD(db, NOME) {
+    override fun cria() {
+        db.execSQL("CREATE TABLE $nome (${BaseColumns._ID} INTEGER PRIMARY KEY AUTOINCREMENT, $nome_evento TEXT NOT NULL, $data TEXT NOT NULL, $descricao TEXT NOT NULL $id_localidade INTEGER NOT NULL, FOREIGN KEY ($id_localidade._ID) REFERENCES ${TabelaBDLocalidade.NOME}(${BaseColumns._ID}) ON DELETE RESTRICT) $organizador_id INTEGER NOT NULL, FOREIGN KEY ($organizador_id._id) REFERENCES ${TabelaBDOrganizador.NOME}(${BaseColumns._ID}) ON DELETE RESTRICT)")
+    }
+
+    companion object {
+        const val NOME = "Eventos"
+        const val nome_evento = "Nome_Evento"
+        const val data = "Data"
+        const val descricao = "descricao"
+        const val id_localidade = "localidade_id"
+        const val organizador_id = "organizador_id"
+    }
 }
--- app/src/main/java/pt/ipg/appevent/TabelaBDOrganizador.kt
+++ app/src/main/java/pt/ipg/appevent/TabelaBDOrganizador.kt
@@ -5,14 +5,15 @@ import android.provider.BaseColumns
 
 class TabelaBDOrganizador(db: SQLiteDatabase) : TabelaBD(db, NOME) {
     override fun cria() {
-            db.execSQL("CREATE TABLE $nome(${BaseColumns._ID} INTEGER PRIMARY KEY AUTOINCREMENT, $Idade TEXT NOT NULL, $telemovel TEXT NOT NULL, $email TEXT NOT NULL $evento_id INTEGER NOT NULL, FOREIGN KEY ($Evento._ID) REFERENCES ${TabelaBDEvento.NOME}(${BaseColumns._ID}) ON DELETE RESTRICT) $Localidade_id INTEGER NOT NULL, FOREIGN KEY ($Localidade._ID) REFERENCES ${TabelaBDLocalidade.NOME}(${BaseColumns._ID}) ON DELETE RESTRICT)")
+            db.execSQL("CREATE TABLE $nome(${BaseColumns._ID} INTEGER PRIMARY KEY AUTOINCREMENT,$nome_organizador TEXT NOT NULL, $Idade TEXT NOT NULL, $telemovel TEXT NOT NULL, $Email TEXT NOT NULL $evento_id INTEGER NOT NULL, FOREIGN KEY ($Evento._ID) REFERENCES ${TabelaBDEvento.NOME}(${BaseColumns._ID}) ON DELETE RESTRICT) $Localidade_id INTEGER NOT NULL, FOREIGN KEY ($Localidade._ID) REFERENCES ${TabelaBDLocalidade.NOME}(${BaseColumns._ID}) ON DELETE RESTRICT)")
         }
 
         companion object {
             const val NOME = "Organizador"
+            const val nome_organizador = "Organizador"
             const val Idade = "idade"
             const val telemovel = "telemovel"
-            const val email = "email"
+            const val Email = "email"
             const val Localidade_id = "Localidade_id"
             const val evento_id = "evento_id"
 
