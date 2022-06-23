ca2f4c06aee3ace7a9ef2b746782116f312c7d8e
vasco <vasco144oliveira@gmail.com>
2022-06-23 18:32:16 +0100

    cursor Evento

--- app/src/main/java/pt/ipg/appevent/Evento.kt
+++ app/src/main/java/pt/ipg/appevent/Evento.kt
@@ -1,14 +1,16 @@
 package pt.ipg.appevent
 
 import android.content.ContentValues
+import android.database.Cursor
+import android.provider.BaseColumns
 
 data class Evento (
     var Nome_Evento : String,
     var Data: String,
     var Descricao: String,
-    var idorganizador: Long,
-    var idlocalidade: Long,
-    var id_tipo_eventos: Long,
+    var organizador: Organizador,
+    var localidade: Localidade,
+    var tipo_eventos: TipoEventos,
     var id: Long = -1
 ) {
     fun toContentValues() : ContentValues {
@@ -17,11 +19,43 @@ data class Evento (
         valores.put(TabelaBDEvento.NOME_EVENTO, Nome_Evento)
         valores.put(TabelaBDEvento.DATA, Data)
         valores.put(TabelaBDEvento.DESCRICAO, Descricao)
-        valores.put(TabelaBDEvento.ORGANIZADOR_ID, idorganizador)
-        valores.put(TabelaBDEvento.LOCALIDADE_ID, idlocalidade)
-        valores.put(TabelaBDEvento.TIPO_EVENTOS_ID, id_tipo_eventos)
+        valores.put(TabelaBDEvento.ORGANIZADOR_ID, organizador.id)
+        valores.put(TabelaBDEvento.LOCALIDADE_ID, localidade.id)
+        valores.put(TabelaBDEvento.TIPO_EVENTOS_ID, tipo_eventos.id)
 
 
         return valores
+    }
+    companion object{
+        fun fromCursor(cursor: Cursor): Evento {
+            val posId = cursor.getColumnIndex(BaseColumns._ID)
+            val posNom = cursor.getColumnIndex(TabelaBDEvento.NOME_EVENTO)
+            val posData = cursor.getColumnIndex(TabelaBDEvento.DATA)
+            val posDesc = cursor.getColumnIndex(TabelaBDEvento.DESCRICAO)
+            val posIdOrganizador = cursor.getColumnIndex(TabelaBDEvento.ORGANIZADOR_ID)
+            val posIdLocalidade = cursor.getColumnIndex(TabelaBDEvento.LOCALIDADE_ID)
+            val posIdTipoEventos = cursor.getColumnIndex(TabelaBDEvento.TIPO_EVENTOS_ID)
+            val posNomeOrganizador = cursor.getColumnIndex(TabelaBDOrganizador.NOME_ORGANIZADOR)
+            val posNomeLocalidade = cursor.getColumnIndex(TabelaBDLocalidade.NOME_LOCALIDADE)
+            val posNomeTipoEvento = cursor.getColumnIndex(TabelaBDTipoEvento.TIPO_EVENTO)
+
+
+            val id = cursor.getLong(posId)
+            val nome = cursor.getString(posNom)
+            val data = cursor.getString(posData)
+            val descricao = cursor.getString(posDesc)
+            val idOrganizador = cursor.getInt(posIdOrganizador)
+            val idLocalidade = cursor.getLong(posIdLocalidade)
+            val idTipoEvento = cursor.getLong(posIdTipoEventos)
+            val nomeOrganizador = cursor.getString(posNomeOrganizador)
+            val nomeLocalidade = cursor.getString(posNomeLocalidade)
+            val nomeTipoEvento = cursor.getString(posNomeTipoEvento)
+
+            val organizador = Organizador(nomeOrganizador,idOrganizador)//pergguntar ao prfessor se e preciso adicionar os outros parametros
+            val localidade =  Localidade(nomeLocalidade,idLocalidade)
+            val tipo_evento = TipoEventos(nomeTipoEvento,idTipoEvento)
+
+            return Evento (nome,data,descricao,organizador,localidade,tipo_evento,id)
+        }
     }
 }
