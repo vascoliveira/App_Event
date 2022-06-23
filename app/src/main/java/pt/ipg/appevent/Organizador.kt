21ce3a0bae20608769148ecf97eb9371eddb8799
vasco <vasco144oliveira@gmail.com>
2022-06-23 17:58:48 +0100

    cursor Organizador

--- app/src/main/java/pt/ipg/appevent/Organizador.kt
+++ app/src/main/java/pt/ipg/appevent/Organizador.kt
@@ -1,6 +1,8 @@
 package pt.ipg.appevent
 
 import android.content.ContentValues
+import android.database.Cursor
+import android.provider.BaseColumns
 
 data class Organizador (
     var Nome_organizador : String,
@@ -17,5 +19,26 @@ data class Organizador (
         valores.put(TabelaBDOrganizador.EMAIL, email)
 
         return valores
+    }
+
+    companion object {
+        fun fromCursor(cursor: Cursor): Organizador {
+            val posId = cursor.getColumnIndex(BaseColumns._ID)
+            val posNome = cursor.getColumnIndex(TabelaBDOrganizador.NOME)
+            val posEmail = cursor.getColumnIndex(TabelaBDOrganizador.EMAIL)
+            val posIdade = cursor.getColumnIndex(TabelaBDOrganizador.IDADE)
+            val posTelemovel = cursor.getColumnIndex(TabelaBDOrganizador.TELEMOVEL)
+
+
+
+
+            val id = cursor.getLong(posId)
+            val nome = cursor.getString(posNome)
+            val email = cursor.getString(posEmail)
+            val idade = cursor.getInt(posIdade)
+            val telemovel = cursor.getString(posTelemovel)
+
+            return Organizador(nome,idade,telemovel,email,id)
+        }
     }
 }
