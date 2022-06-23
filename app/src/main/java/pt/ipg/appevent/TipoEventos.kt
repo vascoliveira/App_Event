4e191c0ba835b7ad011cda8f9b38bdb690ffe6a4
vasco <vasco144oliveira@gmail.com>
2022-06-23 17:48:51 +0100

    cursor TipoEventos

--- app/src/main/java/pt/ipg/appevent/TipoEventos.kt
+++ app/src/main/java/pt/ipg/appevent/TipoEventos.kt
@@ -1,17 +1,31 @@
 package pt.ipg.appevent
 
 import android.content.ContentValues
+import android.database.Cursor
+import android.provider.BaseColumns
 
 data class TipoEventos (
     var tipo_de_evento : String,
     var id: Long = -1
 ) {
-    fun toContentValues() : ContentValues {
+    fun toContentValues(): ContentValues {
         val valores = ContentValues()
 
         valores.put(TabelaBDTipoEvento.TIPO_EVENTO, tipo_de_evento)
 
 
         return valores
+    }
+
+    companion object {
+        fun fromCursor(cursor: Cursor): TipoEventos {
+            val posId = cursor.getColumnIndex(BaseColumns._ID)
+            val posNome = cursor.getColumnIndex(TabelaBDTipoEvento.TIPO_EVENTO)
+
+            val id = cursor.getLong(posId)
+            val nome = cursor.getString(posNome)
+
+            return TipoEventos(nome, id)
+        }
     }
 }
